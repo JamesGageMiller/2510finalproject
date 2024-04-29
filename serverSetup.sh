08ce6e24a1bca497f7d9b839845fc101925feba1
JamesGageMiller <jamesgagemiller@gmail.com>
2024-04-29 20:25:50 +0000

    updated serverSetup.sh
    finished packages loop

--- serverSetup.sh
+++ serverSetup.sh
@@ -12,22 +12,29 @@ arrTickets=$(curl ${strUrl} | jq -r)
 #debug statement
 #echo $arrTickets
 #int to keep track of the index of the while loop
-intIndex=0
+intCurrent=0
 intTickets=$(echo ${arrTickets}| jq "length")
+#iterates through the array for the amount of tickets
 while [ "$intCurrent" -lt "$intTickets" ]; do
+	#checks each ticket to see if its equal to the inputed id
 	if( $strTicketId == $[echo ${arrTickets} | jq -r .[$intCurrent].ticketID] ); then
+		#grabs all of the desired info from the json array
 		strRequestor=$(echo ${arrResults} | jq -r .[$intCurrent].requestor)
 		strSubmissionDate=$(echo ${arrResults} | jq -r .[$intCurrent].submissionDate)
 		strSoftwarePackages=$(echo ${arrResults} | jq -r .[$intCurrent].softwarePackages)
 		strStandardConfig=$(echo ${arrResults} | jq -r .[$intCurrent].standardConfig)
 		strStartTime=$(date + "%Y-%M-%D %H:%M:%S")
+		#sets up each config and sends it  to the log
 		for config in $(echo ${arrTickets} | jq -r .[${intCurrent}].additionalConfigs[].config); do
 			strConfig=$(echo {$arrTickets} | jq -r .[${intCurrent}].additionalConfigs[].name)
-			strConfig >> configurationLogs/${strTicketId}.log 
+			$strConfig >> configurationLogs/${strTicketId}.log 
 			eval $config
 		done
+		#sets up each package and sends it to the log
 		for package in $(echo ${arrTickets} | jq -r .[${intCurrent}].softwarePackages[].install); do
-			
+			strPackage=$(echo ${arrTickets} | jq -r .[${intCurrent}].softwarePackages[].name)
+			$strPackage >> configurationLogs/${strTicketId}.log
+			yes | sudo apt-get install ${package}
 		done
 	fi
 	(($intCurrent++))
