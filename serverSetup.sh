b2631d3e7e016168303ed0a6cd1405ee5955bc2e
JamesGageMiller <jamesgagemiller@gmail.com>
2024-04-29 23:19:56 +0000

    updated serverSetup.sh
    Realised i was using the wrong name for the array in all of my statements that use jq, swapped it and everything magically fixed itself, started the framework for adding stuff to directories if its the requested additonal config

--- serverSetup.sh
+++ serverSetup.sh
@@ -21,31 +21,41 @@ while [ "$intCurrent" -lt "$intTickets" ]; do
 	#checks each ticket to see if its equal to the inputed id
 	if [ ${strTicketId} = $(echo ${arrTickets} | jq -r .[${intCurrent}].ticketID) ]; then
 		#grabs all of the desired info from the json array
-		strRequestor=$(echo ${arrResults} | jq -r .[$intCurrent].requestor)
-		strSubmissionDate=$(echo ${arrResults} | jq -r .[$intCurrent].submissionDate)
-		strSoftwarePackages=$(echo ${arrResults} | jq -r .[$intCurrent].softwarePackages)
-		strStandardConfig=$(echo ${arrResults} | jq -r .[$intCurrent].standardConfig)
+		strRequestor=$(echo ${arrTickets} | jq -r .[$intCurrent].requestor)
+		strSubmissionDate=$(echo ${arrTickets} | jq -r .[$intCurrent].submissionDate)
+		strSoftwarePackages=$(echo ${arrTickets} | jq -r .[$intCurrent].softwarePackages)
+		strStandardConfig=$(echo ${arrTickets} | jq -r .[$intCurrent].standardConfig)
 		strStartTime=$(date + "%Y-%m-%d %H:%M:%S")
 		echo "TicketID: $strTicketId" >> configurationLogs/${strTicketId}.log 
 		#insert starttime log output
 		echo "Requestor: $strRequestor" >> configurationLogs/${strTicketId}.log
 		echo "External IP Address: $strIP" >> configurationLogs/${strTicketId}.log
 		echo "Hostname: $strHostName" >> configurationLogs/${strTicketId}.log
-		echo "Standard Configuration: strStandardConfig" >> configurationLogs/${strTicketId}.log
+		echo "Standard Configuration: $strStandardConfig" >> configurationLogs/${strTicketId}.log
+		echo " " >> configurationLogs/${strTicketId}.log
 		#sets up each config and sends it  to the log
 		for config in $(echo ${arrTickets} | jq -r .[${intCurrent}].additionalConfigs[].config); do
 			strConfig=$(echo {$arrTickets} | jq -r .[${intCurrent}].additionalConfigs[$intConfigIter].name)
-			echo"additonalConfig - $strConfig" >> configurationLogs/${strTicketId}.log
+			echo "additonalConfig - $strConfig" >> configurationLogs/${strTicketId}.log
+			if [ $(echo ${strConfig} | grep -w "touch") = "touch" ]; then
+				strTempDir = $(echo ${strConfig} | cut -d "touch")
+				sudo mkdir -p ${strTempDir} 
+			fi
 			eval $config
+			((intConfigIter++))
 		done
+		echo " " >> configurationLogs/${strTicketId}.log
 		#sets up each package and sends it to the log
 		for package in $(echo ${arrTickets} | jq -r .[${intCurrent}].softwarePackages[].install); do
 			strPackage=$(echo ${arrTickets} | jq -r .[${intCurrent}].softwarePackages[$intPackageIter].name)
-			echo $strPackage
 			echo "softwarePackage - $strPackage" >> configurationLogs/${strTicketId}.log
 			yes | sudo apt-get install ${package}
 			((intPackageIter++))
 		done
+		echo " " >> configurationLogs/${strTicketId}.log
+		echo "TicketClosed" >> configurationLogs/${strTicketId}.log
+		echo " " >> configurationLogs/${strTicketId}.log
+		echo "Completed %Y-%m-%d %H:%M" >> configurationLogs/${strTicketId}.log
 	fi
 	((intCurrent++))
 done
