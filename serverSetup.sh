f73bcef223f6bb0286a1176405aab17b00f20832
JamesGageMiller <jamesgagemiller@gmail.com>
2024-04-29 19:20:02 +0000

    updated serverSetup.sh
    made it grab everything from the json file and mkdir configurationLogs

--- serverSetup.sh
+++ serverSetup.sh
@@ -1,28 +1,26 @@
 #! /bin/bash
 
-#grabs the 
+#grabs the URL, and inputed parameters
+mkdir -p configurationLogs
 strUrl="https://www.swollenhippo.com/ServiceNow/systems/devTickets.php"
 strIP=$1
 strTicketId=$2
-
+touch configurationLogs/${strTicketId}.log
+strHostName=$(hostname)
 # get the array from the given url
 arrTickets=$(curl ${strUrl} | jq -r)
 #debug statement
 #echo $arrTickets
-intTickets = 0
-for "ticketID" in "$arrTickets"; do
-	(($intTickets++))
-done
-#debug stateent
-echo $intTickets
-#initilize two ints, one to say that 
-intDesiredTicket=0
-intCurrent=0
-while [ "$intCurrent" -lt "$intTickets" ]; do
-	if( $strTicketId == $[echo ${arrTickets} | jq -r .[$intCurrent].ticketID] ); then
-		$intDesiredTicket = $intCurrent
+#int to keep track of the index of the while loop
+intIndex=0
+intTickets=$(echo ${arrTickets}| jq "length")
+while [ "$intIndex" -lt "$intTickets" ]; do
+	if( $strTicketId == $[echo ${arrTickets} | jq -r .[$intIndex].ticketID] ); then
+		strRequestor=$(echo ${arrResults} | jq -r .[$intIndex].requestor)
+		strSubmissionDate=$(echo ${arrResults} | jq -r .[$intIndex].submissionDate)
+		strSoftwarePackages=$(echo ${arrResults} | jq -r .[$intIndex].softwarePackages)
+		strStandardConfig=$(echo ${arrResults} | jq -r .[$intIndex].standardConfig)
+		strStartTime=$(date + "%Y-%M-%D %H:%M:%S") 
 	fi
-	(($intCurrent+1))
+	(($intIndex++))
 done
-echo $intDesiredTicket
-
