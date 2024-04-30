4aaf3fe1de38df59cb4a679e4bcdc6d725e6f13b
JamesGageMiller <jamesgagemiller@gmail.com>
2024-04-30 05:19:11 +0000

    final iteration of serverSetup.sh

--- serverSetup.sh
+++ serverSetup.sh
@@ -14,10 +14,11 @@ arrTickets=$(curl ${strUrl} | jq -r)
 #int to keep track of the index of the while loop
 intCurrent=0
 intPackageIter=0
-intConfigIter=0
+intCurrentConfig=0
+intTotalConfigs=0
 intTickets=$(echo ${arrTickets}| jq "length")
 #iterates through the array for the amount of tickets
-while [ "$intCurrent" -lt "$intTickets" ]; do
+while [ ${intCurrent} -lt ${intTickets} ]; do
 	#checks each ticket to see if its equal to the inputed id
 	if [ ${strTicketId} = $(echo ${arrTickets} | jq -r .[${intCurrent}].ticketID) ]; then
 		#grabs all of the desired info from the json array
@@ -25,37 +26,61 @@ while [ "$intCurrent" -lt "$intTickets" ]; do
 		strSubmissionDate=$(echo ${arrTickets} | jq -r .[$intCurrent].submissionDate)
 		strSoftwarePackages=$(echo ${arrTickets} | jq -r .[$intCurrent].softwarePackages)
 		strStandardConfig=$(echo ${arrTickets} | jq -r .[$intCurrent].standardConfig)
-		strStartTime=$(date + "%Y-%m-%d %H:%M:%S")
+		strStartTime=$(date +"%Y-%m-%d %H:%M:%S")
 		echo "TicketID: $strTicketId" >> configurationLogs/${strTicketId}.log 
-		#insert starttime log output
+		echo "Start DateTime: $strStartTime" >> configurationLogs/${strTicketId}.log
 		echo "Requestor: $strRequestor" >> configurationLogs/${strTicketId}.log
 		echo "External IP Address: $strIP" >> configurationLogs/${strTicketId}.log
 		echo "Hostname: $strHostName" >> configurationLogs/${strTicketId}.log
 		echo "Standard Configuration: $strStandardConfig" >> configurationLogs/${strTicketId}.log
 		echo " " >> configurationLogs/${strTicketId}.log
 		#sets up each config and sends it  to the log
-		for config in $(echo ${arrTickets} | jq -r .[${intCurrent}].additionalConfigs[].config); do
-			strConfig=$(echo {$arrTickets} | jq -r .[${intCurrent}].additionalConfigs[$intConfigIter].name)
-			echo "additonalConfig - $strConfig" >> configurationLogs/${strTicketId}.log
-			if [ $(echo ${strConfig} | grep -w "touch") = "touch" ]; then
-				strTempDir = $(echo ${strConfig} | cut -d "touch")
-				sudo mkdir -p ${strTempDir} 
+		#current known bugs:iterates a extra time
+#		for config in $(echo ${arrTickets} | jq -r .[${intCurrent}].additionalConfigs[].config); do
+#			strConfigName=$(echo ${arrTickets} | jq -r .[${intCurrent}].additionalConfigs[$intConfigIter].name)
+#			echo "additonalConfig - $strConfigName" >> configurationLogs/${strTicketId}.log
+#			strConfig=$(echo ${arrTickets} | jq -r .[${intCurrent}].additionalConfigs[$intConfigIter].config)
+#			strArguments=$(echo ${strConfig} | sed 's/^[^[:space:]]* //')
+#			strCommand=$(echo ${strConfig}  | sed 's/ .*//')
+#			if [ $strCommand = "touch" ]; then
+#				strTempDir=$(echo "${strConfig}" | sed 's/^.{0,5}//')
+#				sudo mkdir -p $strTempDir
+#				sudo rm -r $strTempDir
+#			fi
+#			eval "$strCommand $strArguments"
+#			((intConfigIter++))
+#		done
+		strAdditonalConfigs=$(echo ${arrTickets} | jq -r .[${intCurrent}].additionalConfigs)
+		intTotalConfigs=$(echo ${strAdditonalConfigs} | jq length)
+		echo $intTotalConfigs
+		while [ ${intCurrentConfig} -lt ${intTotalConfigs} ];do
+			strConfig=$(echo ${strAdditonalConfigs} | jq .[${intCurrentConfig}])
+			strCommand=$(echo ${strConfig} | jq -r .config)
+			if [[ $strCommand == *"touch"* ]]; then
+				strPath=$(echo ${strCommand})
+				strPath=$(echo $(echo ${strPath}) | sed -e 's/touch //')
+				strPath=$(echo $(echo ${strPath}) | sed -e 's![^/]*$!!')
+				eval $(sudo mkdir -p ${strPath})
 			fi
-			eval $config
-			((intConfigIter++))
+			eval $(sudo ${strCommand})
+			echo "Additonal Config - $(echo ${strConfig} | jq -r .name)  -  $(date +'%s')" >> configurationLogs/${strTicketId}.log
+			((intCurrentConfig++))
 		done
 		echo " " >> configurationLogs/${strTicketId}.log
 		#sets up each package and sends it to the log
 		for package in $(echo ${arrTickets} | jq -r .[${intCurrent}].softwarePackages[].install); do
 			strPackage=$(echo ${arrTickets} | jq -r .[${intCurrent}].softwarePackages[$intPackageIter].name)
-			echo "softwarePackage - $strPackage" >> configurationLogs/${strTicketId}.log
+			echo "softwarePackage - $strPackage  -  $(date +'%s')" >> configurationLogs/${strTicketId}.log
 			yes | sudo apt-get install ${package}
 			((intPackageIter++))
 		done
+		#for package in $(echo ${arrTickets} | jq -r .[${intCurrent}].softwarePackages[].install); do
+		#	${package} --version >> configurationLogs/${strTicketId}.log
+		#done
 		echo " " >> configurationLogs/${strTicketId}.log
 		echo "TicketClosed" >> configurationLogs/${strTicketId}.log
 		echo " " >> configurationLogs/${strTicketId}.log
-		echo "Completed %Y-%m-%d %H:%M" >> configurationLogs/${strTicketId}.log
+		echo "Completed $(date +'%Y-%m-%d %H:%M')" >> configurationLogs/${strTicketId}.log
 	fi
 	((intCurrent++))
 done
